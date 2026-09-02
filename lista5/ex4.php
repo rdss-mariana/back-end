618f92eaef85af432144e0cb15d461a64a15ae38
mariana <marianarosasantos593@gmail.com>
2026-09-02 10:10:45 -0300

    Add files via upload

---
+++ lista5/ex4.php
@@ -0,0 +1,12 @@
+<?php
+
+$letra = "a";
+
+if($letra == "a" || $letra == "e" || $letra == "i" || $letra == "o" || $letra == "u" ){
+    echo "A letra digitada é VOGAL :)";
+}
+else{
+    echo "A letra digitada é CONSOANTE !!!";
+}
+
+?>
