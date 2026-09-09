ccf1d1f7c21e671ed5e8316898b52a48188141ef
mariana <marianarosasantos593@gmail.com>
2026-09-09 17:26:21 -0300

    Add files via upload

---
+++ ex3.php
@@ -0,0 +1,7 @@
+<?php
+
+$gastos_cartao = [100, 500, 600, 900, 2000];
+
+echo "O maior valor gasto no cartão de crédito foi: " . max($gastos_cartao);
+
+?>
---
+++ ex5.php
@@ -0,0 +1,11 @@
+<?php
+
+$a = 32;
+echo "a é numerico? " . is_numeric($a);
+
+echo "<br>";
+
+$b = "Mary";
+echo "b é numérico? " . is_numeric($b);
+
+?>
